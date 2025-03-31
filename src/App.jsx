2ff529aa5fd6e8f499491b37a41ed67ff349d6a4
Nikol Topalusic <ntopalusic@gmail.com>
2025-03-31 13:09:50 +0200

    refactor naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import styles from './App.module.css';
-import { About } from './components_temporename/About/About';
-import { Contact } from './components_temporename/Contact/Contact';
-import { Experience } from './components_temporename/Experience/Experience';
-import { Hero } from './components_temporename/Hero/Hero';
-import { Navbar } from './components_temporename/Navbar/Navbar';
-import { Projects } from './components_temporename/Projects/Projects';
+import { About } from './components/About/About';
+import { Contact } from './components/Contact/Contact';
+import { Experience } from './components/Experience/Experience';
+import { Hero } from './components/Hero/Hero';
+import { Navbar } from './components/Navbar/Navbar';
+import { Projects } from './components/Projects/Projects';
 
 function App() {
   return (
